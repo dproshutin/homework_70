41ce0ba7f1a6dbddb1c6586f11e6a082c078a3ac
Dmitriy Proshutin <dmitry.proshutin@gmail.com>
2019-12-02 15:05:29 +0600

    If-выражения заменены на switch конструкцию в файле reducer.js. Добавлен файл actionTypes.js, в котором определены константы для типов действий.

---
+++ store/actionTypes.js
@@ -0,0 +1,5 @@
+export const ADD = "ADD";
+export const RESULT = "RESULT";
+export const REMOVE = "REMOVE";
+export const REMOVE_ALL = "REMOVE_ALL";
+export const SQUARE = "SQUARE";
--- store/reducer.js
+++ store/reducer.js
@@ -1,34 +1,34 @@
+import {ADD, RESULT, REMOVE, REMOVE_ALL, SQUARE} from "./actionTypes";
+
 const initialState = {
     result: ""
 };
 
 const reducer = (state = initialState, action) => {
-    if (action.type === "ADD") {
-        return {result: state.result + action.value};
-    }
-    if (action.type === "RESULT") {
-        try {
-            const find = '\\^';
-            const re = new RegExp(find, 'g');
-            if (state.result.charCodeAt(0) === 0x221A && state.result.slice(1) >=0) {
-                return {result: Math.sqrt(state.result.slice(1)) + action.value};
+    switch (action.type) {
+        case ADD:
+            return {result: state.result + action.value};
+        case RESULT:
+            try {
+                const find = '\\^';
+                const re = new RegExp(find, 'g');
+                if (state.result.charCodeAt(0) === 0x221A && state.result.slice(1) >=0) {
+                    return {result: Math.sqrt(state.result.slice(1)) + action.value};
+                }
+                return {result: eval(state.result.replace(re, "**")) + action.value};
+            } catch(e) {
+                console.log('Error!');
+                return {result: "ERROR"};
             }
-            return {result: eval(state.result.replace(re, "**")) + action.value};
-        } catch(e) {
-            console.log('Error!');
-            return {result: "ERROR"};
-        }
+        case REMOVE:
+            return {result: state.result.slice(0, -1)};
+        case REMOVE_ALL:
+            return {result: ""};
+        case SQUARE:
+            return {result: eval(state.result) + "^2"};
+        default:
+            return state;
     }
-    if (action.type === "REMOVE") {
-        return {result: state.result.slice(0, -1)};
-    }
-    if (action.type === "REMOVE_ALL") {
-        return {result: ""};
-    }
-    if (action.type === "SQUARE") {
-        return {result: eval(state.result) + "^2"};
-    }
-    return state;
 };
 
 export default reducer;
